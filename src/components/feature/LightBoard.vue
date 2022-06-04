1b32ff0d2dbc30ae156c8a377de5817f50bd38af
shaman771 <shaman771@gmail.com>
2022-06-04 16:09:21 +0300

    feat: implement sequential appear effect

--- src/components/feature/LightBoard.vue
+++ src/components/feature/LightBoard.vue
@@ -4,6 +4,8 @@
       <LightBulb
         v-for="bit in store.bitRange"
         :key="bit.id"
+        :class="{ 'is-visible': !!(showState & bit.weight) }"
+        class="light-board__ramp-item"
         :bit="bit"
         @click="store.toggleLighted(bit.weight)"
       />
@@ -16,13 +18,38 @@
 import LightBoardConsole from './LightBoardConsole';
 import LightBulb from './LightBulb';
 import { useStore } from '../../stores/store';
+import { watchEffect, ref } from 'vue';
 
 const store = useStore();
+const showState = ref(0);
+const timerQueue = [];
+const perUnitDuration = 200;
+
+watchEffect(() => {
+  const totalAppearDuration = perUnitDuration * store.$state.wordSize;
+  showState.value = 0;
+  timerQueue.forEach(timerId => {
+    clearTimeout(timerId);
+  });
+  timerQueue.length = 0;
+  for (let i = 0; i < store.$state.wordSize; i++) {
+    timerQueue.push(setTimeout(() => {
+      showState.value |= Math.pow(2, i);
+    }, 50 + totalAppearDuration - perUnitDuration * i));
+  }
+})
 </script>
 
 <style scoped>
 .light-board__ramp {
   display: flex;
   flex-wrap: wrap;
+}
+.light-board__ramp-item {
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+.light-board__ramp-item.is-visible {
+  opacity: 1;
 }
 </style>
