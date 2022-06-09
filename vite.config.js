f15e249a371a40f9db97d594a1784ea5fb4f79ab
shaman771 <shaman771@gmail.com>
2022-06-09 17:51:25 +0300

    chore: fix env var reference in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
+
+// debug env var reference on build stage
+console.dir(process);
+console.log(process.NODE_ENV);
+
 export default defineConfig({
   base: process.env.MODE === 'production' ? '/binlamps/' : '/',
   plugins: [vue()],
